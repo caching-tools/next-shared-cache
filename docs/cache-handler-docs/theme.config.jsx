649d999198adff4aa9f1479ef490f34020b3eabb
Arseny <132773930+better-salmon@users.noreply.github.com>
2023-11-15 03:22:13 +0300

    Configure SEO and footer for docs

--- docs/cache-handler-docs/theme.config.jsx
+++ docs/cache-handler-docs/theme.config.jsx
@@ -1,7 +1,52 @@
+import { useRouter } from 'next/router';
+import { useConfig } from 'nextra-theme-docs';
+
+function Head() {
+    const { asPath } = useRouter();
+    const { frontMatter } = useConfig();
+    const url = `https://caching-tools.github.io/next-shared-cache${asPath}`;
+
+    return (
+        <>
+            <meta content={url} property="og:url" />
+            <meta content={frontMatter.title || '@neshca/cache-handler'} property="og:title" />
+            <meta
+                content={frontMatter.description || '@neshca/cache-handler documentation'}
+                property="og:description"
+            />
+        </>
+    );
+}
+
 export default {
     logo: <pre>@neshca/cache-handler</pre>,
     project: {
         link: 'https://github.com/caching-tools/next-shared-cache',
     },
     docsRepositoryBase: 'https://github.com/caching-tools/next-shared-cache/tree/canary/docs/cache-handler-docs',
+    useNextSeoProps() {
+        const { asPath } = useRouter();
+
+        if (asPath !== '/') {
+            return {
+                titleTemplate: '%s — @neshca/cache-handler',
+            };
+        }
+
+        return {
+            titleTemplate: '@neshca/cache-handler',
+        };
+    },
+    head: Head,
+    footer: {
+        text: (
+            <span>
+                MIT {new Date().getFullYear()} ©{' '}
+                <a href="https://github.com/caching-tools/next-shared-cache" rel="noopener" target="_blank">
+                    @neshca/cache-handler
+                </a>
+                .
+            </span>
+        ),
+    },
 };
