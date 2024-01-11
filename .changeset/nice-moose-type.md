9dcb3935942c0e48e803bbf884fe04590811a704
Arseny <132773930+better-salmon@users.noreply.github.com>
2024-01-11 14:30:10 +0300

    Refactor `lru-cache` Handler to overcome ttl limitations (#252)

---
+++ .changeset/nice-moose-type.md
@@ -0,0 +1,5 @@
+---
+'@neshca/cache-handler': patch
+---
+
+Refactored `lru-cache` Handler to overcome ttl via setTimeout limitations. Added `timeoutMs` option to `server` Handler.
--- apps/cache-testing/cache-handler-server.js
+++ apps/cache-testing/cache-handler-server.js
@@ -1,17 +1,12 @@
+const { scheduler } = require('node:timers/promises');
 const { IncrementalCache } = require('@neshca/cache-handler');
 const createServerCache = require('@neshca/cache-handler/server').default;
 const createLruCache = require('@neshca/cache-handler/local-lru').default;
 
 const baseUrl = process.env.REMOTE_CACHE_SERVER_BASE_URL ?? 'http://localhost:8080';
 
-async function wait(ms) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, ms);
-    });
-}
-
 IncrementalCache.onCreation(async () => {
-    await wait();
+    await scheduler.wait(1000);
 
     const httpCache = createServerCache({
         baseUrl,
