ccd0f9ccde254ceb5919156c01e06f1ba3b2d116
Arseny <132773930+better-salmon@users.noreply.github.com>
2024-01-19 16:45:52 +0300

    Update Next.js dependencies and cacheHandler configuration

--- apps/cache-testing/next.config.js
+++ apps/cache-testing/next.config.js
@@ -1,14 +1,15 @@
 const path = require('node:path');
 
-const incrementalCacheHandlerPath = require.resolve(process.env.HANDLER_PATH ?? './cache-handler-redis-stack');
+const cacheHandler = require.resolve(process.env.HANDLER_PATH ?? './cache-handler-redis-stack');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     poweredByHeader: false,
     reactStrictMode: true,
     output: 'standalone',
+    cacheHandler: process.env.NODE_ENV !== 'development' ? cacheHandler : undefined,
+    cacheMaxMemorySize: 0, // disable default in-memory caching
     experimental: {
-        incrementalCacheHandlerPath: process.env.NODE_ENV !== 'development' ? incrementalCacheHandlerPath : undefined,
         // PPR should only be configured via the PPR_ENABLED env variable due to conditional logic in tests.
         ppr: process.env.PPR_ENABLED === 'true',
         largePageDataBytes: 1024 * 1024, // 1MB
