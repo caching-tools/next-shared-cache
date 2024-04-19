54764e22a912c249d60c6617a2ad07eb44b642b7
Arseny <132773930+better-salmon@users.noreply.github.com>
2024-04-19 16:45:52 +0300

    Updates types, JSDoc and the documentation

--- apps/cache-testing/cache-handler-redis-stack.mjs
+++ apps/cache-testing/cache-handler-redis-stack.mjs
@@ -3,7 +3,6 @@
 import { CacheHandler } from '@neshca/cache-handler';
 import createLruHandler from '@neshca/cache-handler/local-lru';
 import createRedisHandler from '@neshca/cache-handler/redis-stack';
-import createRedisStringsHandler from '@neshca/cache-handler/redis-strings';
 import { createClient } from 'redis';
 
 CacheHandler.onCreation(async () => {
@@ -11,29 +10,65 @@ CacheHandler.onCreation(async () => {
         console.warn('Make sure that REDIS_URL is added to the .env.local file and loaded properly.');
     }
 
-    const client = createClient({
-        url: process.env.REDIS_URL,
-        name: `cache-handler:${process.env.PORT ?? process.pid}`,
-    });
+    let client;
 
-    client.on('error', () => {});
+    try {
+        // Create a Redis client.
+        client = createClient({
+            url: process.env.REDIS_URL,
+            name: `cache-handler:${process.env.PORT ?? process.pid}`,
+        });
 
-    console.info('Connecting Redis client...');
-    await client.connect();
-    console.info('Redis client connected.');
+        // Redis won't work without error handling.
+        client.on('error', () => {});
+    } catch (error) {
+        console.warn('Failed to create Redis client:', error);
+    }
 
-    const redisHandler = await createRedisHandler({
-        client,
-        keyPrefix: 'JSON:',
-        timeoutMs: 1000,
-    });
+    if (client) {
+        try {
+            console.info('Connecting Redis client...');
 
-    const redisStringsHandler = createRedisStringsHandler({ client, keyPrefix: 'strings:', timeoutMs: 1000 });
+            // Wait for the client to connect.
+            // Caveat: This will block the server from starting until the client is connected.
+            // And there is no timeout. Make your own timeout if needed.
+            await client.connect();
+            console.info('Redis client connected.');
+        } catch (error) {
+            console.warn('Failed to connect Redis client:', error);
 
-    const localHandler = createLruHandler();
+            console.warn('Disconnecting the Redis client...');
+            // Try to disconnect the client to stop it from reconnecting.
+            client
+                .disconnect()
+                .then(() => {
+                    console.info('Redis client disconnected.');
+                })
+                .catch(() => {
+                    console.warn('Failed to quit the Redis client after failing to connect.');
+                });
+        }
+    }
+
+    /** @type {import("@neshca/cache-handler").Handler | null} */
+    let handler;
+
+    if (client?.isReady) {
+        // Create the `redis-stack` Handler if the client is available and connected.
+        handler = await createRedisHandler({
+            client,
+            keyPrefix: 'JSON:',
+            timeoutMs: 1000,
+        });
+    } else {
+        // Fallback to LRU handler if Redis client is not available.
+        // The application will still work, but the cache will be in-memory only and not shared.
+        handler = createLruHandler();
+        console.warn('Falling back to LRU handler because Redis client is not available.');
+    }
 
     return {
-        handlers: [redisHandler, redisStringsHandler, localHandler],
+        handlers: [handler],
         ttl: { defaultStaleAge: 60, estimateExpireAge: (staleAge) => staleAge * 2 },
     };
 });
