238952b9b725ada59b6bf3b4415f5a707a179fc4
jeonggeun-jo <jeonggeun.jo@linecorp.com>
2026-02-01 22:29:46 +0900

    fix: apply keyPrefix in redis-stack delete method

--- packages/cache-handler/src/handlers/redis-stack.ts
+++ packages/cache-handler/src/handlers/redis-stack.ts
@@ -206,7 +206,7 @@ export default function createHandler({
       await client.unlink(options, keysToDelete);
     },
     async delete(key) {
-      await client.unlink(getTimeoutRedisCommandOptions(timeoutMs), key);
+      await client.unlink(getTimeoutRedisCommandOptions(timeoutMs), keyPrefix + key);
     },
   };
 }
